f68c2e73fd6c85c8a8b6cba71c113d6856d0701f
Maiilo <andrescamilorojasrosero@gmail.com>
2023-11-18 16:20:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/DogResponse.kt
@@ -0,0 +1,9 @@
+package com.example.retrofit
+
+import com.google.gson.annotations.SerializedName
+
+data class DogResponse (
+    @SerializedName("status") var status: String,
+    @SerializedName("message") var image: List<String>
+)
+
